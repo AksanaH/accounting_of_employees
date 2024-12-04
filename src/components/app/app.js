2d61412e01e90d2012d6ef3d80cb5bacb0e09489
Aksana <aksa.hleb@gmail.com>
2024-12-03 22:34:59 -0600

    Add filter and employees-list components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,8 @@
 import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../serch-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
 
 function App() {
     return (
@@ -8,7 +10,9 @@ function App() {
             <AppInfo />
             <div className="search-panel">
                 <SearchPanel />
+                <AppFilter />
             </div>
+            <EmployeesList />
         </div>
     )
 }
