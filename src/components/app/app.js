ae9ba61ea9b84fbdb52be1cda606c68ba69e9d07
Aksana <aksa.hleb@gmail.com>
2024-12-17 22:00:58 -0600

    Add employee searching

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,7 +14,8 @@ class App extends Component {
                 { name: 'John Smith', salary: 80000, increase: false, star: false, id: 1 },
                 { name: 'Nansy Parker', salary: 110000, increase: true, star: false, id: 2 },
                 { name: 'Kelly Taylor', salary: 150000, increase: false, star: false, id: 3 }
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4
     }
@@ -98,17 +99,34 @@ class App extends Component {
         }))
     }
 
+    searchEmployee = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({ term });
+    }
+
     render() {
+        const { data, term } = this.state;
         const employees = this.state.data.length;
         const increasedEmployees = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmployee(data, term);
+
         return (
             <div className="app">
                 <AppInfo employees={employees} increasedEmployees={increasedEmployees} />
                 <div className="search-panel">
-                    <SearchPanel />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
                     <AppFilter />
                 </div>
-                <EmployeesList data={this.state.data}
+                <EmployeesList data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleIncrease={this.onToggleIncrease}
                     onToggleStar={this.onToggleStar} />
